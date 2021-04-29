e1e665006e4b29815b3f0ff4d2cc91824fe1ca52
Pavla Krotka <pavla.krotka@gmail.com>
2021-04-29 17:01:43 +0200

    z test update

--- simbootcamp/R/z_test.R
+++ simbootcamp/R/z_test.R
@@ -3,26 +3,26 @@
 #' @description Two-Sample z-test for Comparing Two Means
 #'
 #' @param x,y  vectors of observations
-#' 
+#'
 #' @param sd an assumed common standard deviation
-#' 
-#' @param delta the mean difference corresponding to the null hypothesis  
-#' 
+#'
+#' @param delta the mean difference corresponding to the null hypothesis
+#'
 #' @return List with z-statistics and p-value
 #'
 
 z_test <- function(x, y, sd=1, delta=0){
-  
+
   n_x <- length(x)
   n_y <- length(y)
-  
+
   # calculate the z-statistic
   z_stat <- (mean(x) - mean(y) - delta) /
     sqrt(sd^2/n_x + sd^2/n_y)
-  
-  
-  pvalue <- 1-pnorm(abs(z_stat))
+
+
+  pvalue <- 2*(1-pnorm(abs(z_stat)))
   #decision <- (pvalue < 0.05)
-  
+
   return(list(z_stat=z_stat, p.value=pvalue))
 }
